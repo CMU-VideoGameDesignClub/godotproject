7757ca0fd7006e64432aea9ecfbc6d700ef2a3a9
KlausKingsman <klauskingsman@gmail.com>
2024-02-14 15:49:05 -0700

    background and tweaks

--- main.gd
+++ main.gd
@@ -18,13 +18,17 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_score_timer_timeout():
 	score += 1
@@ -49,11 +53,11 @@ func _on_mob_timer_timeout():
 	mob.position = mob_spawn_location.position
 
 	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
+	direction += randf_range(-PI / 16, PI / 16)
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	var velocity = Vector2(randf_range(550.0, 750.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
